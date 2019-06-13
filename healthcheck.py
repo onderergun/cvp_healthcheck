7825b93c49bea7993be26e21dd2c4982cc067300
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2019-06-13 10:57:13 -0700

    Needs exception handling.

---
+++ healthcheck.py
@@ -0,0 +1,16 @@
+import commands
+import re
+import os
+
+test = commands.getstatusoutput("""su -c 'cvpi status all' cvp""")
+currentStatus = re.findall(r"[0-9]{1,3}\/[0-9]{1,3} components running",str(test))
+primaryCount = currentStatus[0].strip(' components running')
+primary = primaryCount.split('/')
+if primary[0] == primary[1]:
+    primaryStatus = "Ok"
+else:
+    primaryStatus = "Fail"
+
+with open('/cvpi/apps/cvp-frontend/web/api/healthcheck.html','w') as f:
+    f.write(primaryStatus)
+    f.close
